4d333e386b9900a3c542f9a224d90a78639d0a8b
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-07-22 21:53:11 +0530

    create sign up api

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from './routes/userroute.js';
-
+import authRouter from './routes/auth.route.js';
 const app=express();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to Database");
@@ -17,3 +19,4 @@ app.listen(3000,()=>{
 }); 
 
 app.use("/api/user",userRouter);   
+app.use("/api/auth",authRouter);
