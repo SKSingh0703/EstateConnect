12ebd525aa3d831222e008e4c4e282292abf37d4
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-07-24 00:38:17 +0530

    Complete setup of redux store globally

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,10 +2,15 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaSpinner } from "react-icons/fa";
 
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart,signInSuccess,signInFailure } from "../redux/user/userSlice"; 
+
 export default function SignIn() {
+
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+
+  const {loading,error} = useSelector((state) => state.user);
   const [success, setSuccess] = useState(null);
 
   const [formdata, setFormData] = useState({});
@@ -19,7 +24,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "post",
         headers: {
@@ -29,20 +34,17 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
 
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       setSuccess("Login Successful...");
       setTimeout(() => {
         navigate("/");
       }, 2000);
     } catch (error) {
-      setLoading(false);
-      setError(error.message); 
+      dispatch(signInFailure(error.message)); 
     }
   };
 
