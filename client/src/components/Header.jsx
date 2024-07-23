fe172153782d79d7c03c62ae52eb7c6684f6a336
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-07-24 01:15:40 +0530

    Add redux persist

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -43,9 +43,9 @@ export default function Header() {
     <header className='bg-gray-900 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-white'>Estate</span>
-            <span className=' text-gray-400 '>Connect</span>
+          <h1 className='font-bold text-sm sm:text-2xl flex flex-wrap'>
+            <span className='text-white '>Estate</span>
+            <span className=' text-gray-400'>Connect</span>
           </h1>
         </Link>
         <form className='bg-gray-800 p-3 rounded-lg flex items-center'>
