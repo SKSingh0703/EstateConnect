60fc4ed863418a8acaf4a2440eee479c7aa18d21
Sachin Kumar <sachinkumar9031735255@gmail.com>
2024-07-26 15:07:22 +0530

    create listing Api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import mongoose from "mongoose";
 import userRouter from './routes/userroute.js';
 import authRouter from './routes/auth.route.js';
 
+import listingRouter from './routes/listing.route.js';
+ 
 import cookieParser from 'cookie-parser';
 
 const app=express();
@@ -25,6 +27,7 @@ app.listen(3000,()=>{
 
 app.use("/api/user",userRouter);   
 app.use("/api/auth",authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
